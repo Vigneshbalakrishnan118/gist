ede0809327d4e93eabb3d05be569e637f53b6588
gopinath-mallow <gopinath@mallow-tech.com>
2022-03-25 13:52:30 +0530

    changes in require

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
-require "welcome/client"
-require "welcome/campaign"
-require "welcome/contact"
-require "welcome/conversation"
-require "welcome/form"
-require "welcome/segment"
-require "welcome/subscription_type"
-require "welcome/tag"
-require "welcome/team"
-require "welcome/teammate"
+require "./lib/welcome/client"
+require "./lib/welcome/campaign"
+require "./lib/welcome/contact"
+require "./lib/welcome/conversation"
+require "./lib/welcome/form"
+require "./lib/welcome/segment"
+require "./lib/welcome/subscription_type"
+require "./lib/welcome/tag"
+require "./lib/welcome/team"
+require "./lib/welcome/teammate"
+
 require_relative "welcome/version"
 require "net/http"
 
