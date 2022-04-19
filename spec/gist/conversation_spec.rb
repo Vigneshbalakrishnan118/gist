31db8391b483899e13432b2aa7656e53153b3678
Vignesh Balakrishan <vignesh@mallow-tech.com>
2022-04-19 16:08:41 +0530

    Changes on gem spec status codes

--- spec/gist/conversation_spec.rb
+++ spec/gist/conversation_spec.rb
@@ -5,7 +5,7 @@ require "gist/conversation"
 
 RSpec.describe Gist::Conversation do
   before(:all) do
-    @client = Gist::Client.new(access_token: "Sp8dwaxjE7NqcfMm0X2jOSK58hvvkpZGTnou5eHA9KTIQ/hlmX5U60kZx95kdMrvkqo=")
+    @client = Gist::Client.new(access_token: "Bearer 2rHqyAVcuGNdwWEs7IPABm/EmrkbyOzrW55DKHqQkaaL3CVjvV+bdwcd+wwm7sTGvLg=")
     @conversation = @client.conversations.create(params: example_create_conversation)
     @conversation_details = JSON.parse(@conversation.body)
     @messages = @client.conversations.find_all_messages(id: @conversation_details["conversation"]["id"])
@@ -15,14 +15,14 @@ RSpec.describe Gist::Conversation do
   describe "create conversation" do
     context "with valid parameters" do
       it "should create" do
-        expect(@conversation[:status]).to eq("200 OK")
+        expect(@conversation.code).to eq("200")
       end
     end
 
     context "with valid parameters" do
       it "shouldn't create" do
         response = @client.conversations.create(params: example_invalid_conversation)
-        expect(response[:status]).to eq("422 Unprocessable Entity")
+        expect(response.code).to eq("422")
       end
     end
   end
@@ -30,20 +30,20 @@ RSpec.describe Gist::Conversation do
   describe "update conversation" do
     it "should update" do
       response = @client.conversations.update(id: @conversation_details["conversation"]["id"], params: example_update_conversation)
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 
   describe "find conversation" do
     it "should find with id" do
       response = @client.conversations.find(id: @conversation_details["conversation"]["id"])
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 
   describe "find messages" do
     it "should find all messages" do
-      expect(@messages[:status]).to eq("200 OK")
+      expect(@messages.code).to eq("200")
     end
   end
 
@@ -52,13 +52,13 @@ RSpec.describe Gist::Conversation do
       puts @messages_details['messages'][0]['id']
       response = @client.conversations.apply_tag(id: @conversation_details["conversation"]["id"],
                                                  params: { "message_id": @messages_details['messages'][0]['id'], "tags": "new tag" })
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
 
     it "should remove tag" do
       response = @client.conversations.remove_tag(id: @conversation_details["conversation"]["id"],
                                                   params: { "message_id": @messages_details['messages'][0]['id'], "tag_id": 328 })
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 
@@ -66,14 +66,14 @@ RSpec.describe Gist::Conversation do
     context "with valid parameters" do
       it "should change status" do
         response = @client.conversations.change_status(id: @conversation_details["conversation"]["id"], params: example_change_status)
-        expect(response[:status]).to eq("200 OK")
+        expect(response.code).to eq("200")
       end
     end
 
     context "with valid parameters" do
       it "shouldn't change status" do
         response = @client.conversations.change_status(id: @conversation_details["conversation"]["id"], params: invalid_change_status)
-        expect(response[:status]).to eq("422 Unprocessable Entity")
+        expect(response.code).to eq("422")
       end
     end
   end
@@ -81,12 +81,12 @@ RSpec.describe Gist::Conversation do
   describe "conversation assignment" do
     it "should assign conversation" do
       response = @client.conversations.assign(id: @conversation_details["conversation"]["id"], params: example_assign)
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
 
     it "should unassign conversation" do
       response = @client.conversations.unassign(id: @conversation_details["conversation"]["id"], params: example_unassign)
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 
@@ -94,14 +94,14 @@ RSpec.describe Gist::Conversation do
     context "with valid parameters" do
       it "should reply" do
         response = @client.conversations.add_reply(id: @conversation_details["conversation"]["id"], params: example_reply)
-        expect(response[:status]).to eq("200 OK")
+        expect(response.code).to eq("200")
       end
     end
 
     context "with valid parameters" do
       it "shouldn't reply" do
         response = @client.conversations.add_reply(id: @conversation_details["conversation"]["id"], params: example_invalid_reply)
-        expect(response[:status]).to eq("422 Unprocessable Entity")
+        expect(response.code).to eq("422")
       end
     end
   end
@@ -109,31 +109,31 @@ RSpec.describe Gist::Conversation do
   describe "delete conversation" do
     it "should delete" do
       response = @client.conversations.delete(id: @conversation_details["conversation"]["id"])
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 
   describe "conversation index" do
     it "should be listable" do
       response = @client.conversations.find_all
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 
   describe "conversation count" do
     it "should get global count" do
       response = @client.conversations.global_counts
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
 
     it "should get teammates count" do
       response = @client.conversations.teammates_count
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
 
     it "should get teams count" do
       response = @client.conversations.teams_count
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 end
--- spec/gist/team_spec.rb
+++ spec/gist/team_spec.rb
@@ -5,20 +5,20 @@ require "gist/team"
 
 RSpec.describe Gist::Team do
   before(:all) do
-    @client = Gist::Client.new(access_token: "Sp8dwaxjE7NqcfMm0X2jOSK58hvvkpZGTnou5eHA9KTIQ/hlmX5U60kZx95kdMrvkqo=")
+    @client = Gist::Client.new(access_token: "Bearer 2rHqyAVcuGNdwWEs7IPABm/EmrkbyOzrW55DKHqQkaaL3CVjvV+bdwcd+wwm7sTGvLg=")
   end
 
   describe "find team" do
     it "should find with id" do
       response = @client.teams.find(id: 1)
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 
   describe "team index" do
     it "should be listable" do
       response = @client.teams.find_all
-      expect(response[:status]).to eq("200 OK")
+      expect(response.code).to eq("200")
     end
   end
 end
