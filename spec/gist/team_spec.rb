b3f59b78375d657e517651677afa8f9c2d48ef36
Vignesh Balakrishan <vignesh@mallow-tech.com>
2022-04-20 11:04:26 +0530

    changes to directly access API_KEY

--- spec/gist/team_spec.rb
+++ spec/gist/team_spec.rb
@@ -5,7 +5,7 @@ require "gist/team"
 
 RSpec.describe Gist::Team do
   before(:all) do
-    @client = Gist::Client.new(access_token: "Bearer #{ENV['API_KEY']}")
+    @client = Gist::Client.new(access_token: ENV['API_KEY'])
   end
 
   describe "find team" do
